f2febc989c20691f7ec100e8a370c2f49fc875fe
liak <liakitori@mail.ru>
2025-05-19 00:23:56 +0400

    Доработал класс User, а именно: 1) поправил required и [Required] 2) исправил мелкие ошибки и недочеты

--- ATON_Internship/Models/User.cs
+++ ATON_Internship/Models/User.cs
@@ -6,27 +6,40 @@ namespace ATON_Internship.Models
     public class User
     {
         [Required]
-        public Guid Guid { get; set; }
+        public required Guid Guid { get; set; }
 
         [Required]
         [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "Логин может содержать только латинские буквы и цифры.")]
-        public string Login { get; set; }
+        public required string Login { get; set; }
 
+        [Required]
         [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "Пароль может содержать только латинские буквы и цифры.")]
-        public string Password { get; set; }
+        public required string Password { get; set; }
 
-        [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "Имя может содержать только латинские буквы и цифры.")]
-        public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Zа-яА-ЯёЁ]+$", ErrorMessage = "Имя может содержать только латинские буквы и цифры.")]
+        public required string Name { get; set; }
 
-        [Range(0,2, ErrorMessage = "Пол может иметь следующие значения: 0/1/2, где 0 - женщина, 1 - мужчина или 2 - неизвестно.")]
-        public int Gender { get; set; }
+        [Required]
+        [Range(0,2, ErrorMessage = "Пол может иметь только следующие значения: 0 (женщина), 1 (мужчина) или 2 (неизвестно).")]
+        public required int Gender { get; set; }
 
         public DateTime? Birthday { get; set; }
+
         public bool Admin { get; set; }
-        public DateTime CreatedOn { get; set; }
-        public string CreatedBy { get; set; }
-        public DateTime ModifiedOn { get; set; }
-        public string ModifiedBy { get; set; }
+
+        [Required]
+        public required DateTime CreatedOn { get; set; }
+
+        [Required]
+        public required string CreatedBy { get; set; }
+
+        [Required]
+        public required DateTime ModifiedOn { get; set; }
+
+        [Required]
+        public required string ModifiedBy { get; set; }
+
         public DateTime? RevokedOn { get; set; }
         public string? RevokedBy { get; set; }
     }
