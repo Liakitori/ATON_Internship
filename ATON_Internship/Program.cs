949f48843b9163e7660c74b7c2928cd3ba711072
Liakitori <liakitori@mail.ru>
2025-05-18 18:36:53 +0400

    Добавил недостающие пакеты

--- ATON_Internship/Program.cs
+++ ATON_Internship/Program.cs
@@ -12,6 +12,9 @@ namespace ATON_Internship
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            
 
             var app = builder.Build();
 
@@ -19,6 +22,8 @@ namespace ATON_Internship
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
