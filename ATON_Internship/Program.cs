488f1985e1cb470e36ed2259e8bd3d51bd094db5
liak <liakitori@mail.ru>
2025-05-19 08:11:04 +0400

    Добавляем чтение настроек JWT

--- ATON_Internship/Program.cs
+++ ATON_Internship/Program.cs
@@ -1,4 +1,6 @@
 
+using ATON_Internship.Models;
+
 namespace ATON_Internship
 {
     public class Program
@@ -7,10 +9,14 @@ namespace ATON_Internship
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            //Читаем настройки JWT
+            builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("Jwt"));
+
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             // Add services to the container.
 
+
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
